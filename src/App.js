11fa5e7c83321a4aa4c40a16fb2dd9ebcbfe3c59
Teja <makkenabrothers143@gmail.com>
2021-12-20 09:21:38 +0530

    First Message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react'
+import HomeComponent from './components/HomeComponent';
+import ResultComponent from './components/ResultComponent';
+import QuizComponent from './components/QuizComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomeComponent/>
+      <QuizComponent/>
+      <ResultComponent/>
     </div>
   );
 }
